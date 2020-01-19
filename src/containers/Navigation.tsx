15ef843652ed218f972ab1c1cb0f1448c4561d6a
Nikita Kovalchuk <nko@dvhb.ru>
2020-01-19 16:18:36 +0300

    Change Navigations style

--- src/containers/Navigation.tsx
+++ src/containers/Navigation.tsx
@@ -3,25 +3,29 @@ import { NavLink } from 'react-router-dom';
 
 export const Navigation: React.FC = () => {
     return (
-        <div>
-            <NavLink
-                exact to="/"
-                activeStyle={{
-                    textDecoration: 'none',
-                    color: 'black'
-                }}
-            >
-                Home
-            </NavLink>
-            <NavLink
-                exact to="/profile"
-                activeStyle={{
-                    textDecoration: 'none',
-                    color: 'black'
-                }}
-            >
-                Profile
-            </NavLink>
-        </div>
+        <ul className="filters">
+            <li>
+                <NavLink
+                    exact to="/"
+                    activeStyle={{
+                        textDecoration: 'none',
+                        color: 'black'
+                    }}
+                >
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    exact to="/profile"
+                    activeStyle={{
+                        textDecoration: 'none',
+                        color: 'black'
+                    }}
+                >
+                    Profile
+                </NavLink>
+            </li>
+        </ul>
     );
 };
